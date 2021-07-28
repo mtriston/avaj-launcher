19682539ba2b0be361d073c859aaa62091956d2a
Spivak Alexander <po4ta.polevaya@gmail.com>
2021-07-28 22:48:10 +0300

    Исключение при несуществующей погоде

---
+++ src/main/java/ro/academyplus/avaj/exceptions/IllegalWeatherException.java
@@ -0,0 +1,7 @@
+package ro.academyplus.avaj.exceptions;
+
+public class IllegalWeatherException extends RuntimeException {
+    public IllegalWeatherException(String message) {
+        super(message);
+    }
+}
