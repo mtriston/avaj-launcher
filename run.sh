b4892b66412063d9c6041470116f289f4ae841b8
Spivak Alexander <po4ta.polevaya@gmail.com>
2021-07-29 23:40:13 +0300

    Основной класс с методом main

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ find * -name "*.java" > sources.txt
 mkdir -p target
 javac -d target @sources.txt
 
-java -cp target ro.academyplus.avaj.app.Program scenario
+java -cp target ro.academyplus.avaj.app.Program scenario.txt
---
+++ src/main/java/ro/academyplus/avaj/app/Program.java
@@ -0,0 +1,22 @@
+package ro.academyplus.avaj.app;
+
+import ro.academyplus.avaj.simulator.Simulator;
+
+import java.io.FileNotFoundException;
+
+import ro.academyplus.avaj.parser.Parser;
+
+public class Program {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Expected path to file with scenario.");
+            System.exit(-1);
+        }
+        try {
+            Simulator simulator = new Parser().parseScenario(args[0]);
+            simulator.run();
+        } catch (FileNotFoundException e) {
+            System.err.println("Invalid scenario file: " + e.getMessage());
+        }
+    }
+}
